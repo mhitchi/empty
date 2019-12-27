04f337379c9d4c642aed3aab44715e6de88dadf9
mhitchi <mayhitchings@gmail.com>
2019-12-27 16:30:28 -0500

    fixed .toLowerCase methods, picked random character in full charSet range

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 //ask for length
-var length = prompt("How many characters will your password be? Enter a number between 8 and 128");
+var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
 
 //ask for character type
 var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
@@ -11,22 +11,23 @@ var charType = prompt("Enter a character type: special, numeric, uppercase, lowe
 function generatePassword() {
   //evaluate character type
   var charSet = "";
-  if( charType.toLowerCase === "lowercase" ) {
+  var charLower = charType.toLowerCase();
+  if( charLower === "lowercase" ) {
     charSet = "abcdefghijklmnopqrstuvwxyz";
-  } else if( charType.toLowerCase === "uppercase" ) {
+  } else if( charLower === "uppercase" ) {
     charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( charType.toLowerCase === "numeric" ) {
+  } else if( charLower === "numeric" ) {
     charSet = "0123456789";
-  } else if( charType.toLowerCase === "special" ) {
+  } else if( charLower === "special" ) {
     charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   } 
   //return value
   var retVal = "";
   //for (var i = 0, n = charSet.length; i < length; i++) {
-    for (var i = 0, n = length; i < length; i++) {
+    for (var i = 0; i < length; i++) {
     //picks a character within charSet at index of random number
-    //retVal += charSet.charAt(Math.floor(Math.random() * n));
-    retVal += charSet[Math.floor(Math.random() * n)];
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    //retVal += charSet[Math.floor(Math.random() * n)];
   }
   console.log(retVal);
   return retVal;
